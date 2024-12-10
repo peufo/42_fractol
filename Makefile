8d341aad58aec47ef10d95bfd3156cd5bb21fbe6
Jonas <jonas.voisard@gmail.com>
2024-12-10 13:33:56 +0100

    initial setup

---
+++ Makefile
@@ -0,0 +1,38 @@
+NAME			=	fractol
+DIR_SRC			=	./src
+DIR_BUILD		=	./build
+SOURCES			=	main.c 
+SOURCES_NAME	=	$(basename $(SOURCES))
+OBJECTS			=	$(addsuffix .o, $(addprefix $(DIR_BUILD)/, $(SOURCES_NAME)))
+FLAGS			=	-Wall -Wextra -Werror
+
+FT_PRINTF		=	./lib/ft_printf
+LIBX			=	./lib/minilibx-linux
+LIBS_INCLUDE	=	-I$(FT_PRINTF) -I$(LIBX) -I/usr/include
+LIBS_LINK_DIR	=	-L$(FT_PRINTF) -L$(LIBX) -L/usr/lib  
+LIBS_LINK		=	-lftprintf -lmlx -lXext -lX11 -lm -lz
+
+all: $(NAME)
+
+bonus: $(NAME)
+
+$(NAME): $(OBJECTS)
+	@make -C $(FT_PRINTF)
+	@cc $(OBJECTS) $(FLAGS) $(LIBS_LINK_DIR) $(LIBS_LINK) -o $@
+
+$(DIR_BUILD)/%.o: $(DIR_SRC)/%.c | $(DIR_BUILD)
+	@cc $(FLAGS) $(LIBS_INCLUDE) -c $^ -o $@
+
+$(DIR_BUILD):
+	@mkdir -p $@
+
+clean:
+	make clean -C $(FT_PRINTF)
+	rm -rf $(DIR_BUILD)
+
+fclean: clean
+	make fclean -C $(FT_PRINTF)
+	rm -f $(NAME)
+
+re: fclean all
+	make re -C $(FT_PRINTF)
---
+++ w.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+info() {
+	echo -e "\033[36m$1\033[0m"
+}
+warning() {
+	echo -e "\033[31m$1\033[0m"
+}
+success() {
+	echo -e "\033[32m$1\033[0m"
+}
+
+watch() {
+	STATE_A=""
+	while [[ true ]]
+	do
+		STATE_B=$(get_state)
+		if [[ $STATE_A != $STATE_B ]]; then
+			STATE_A=$STATE_B
+			clear
+			info "HEY BRO 👋 $(date)"
+			./sync.sh
+
+			NORM_ERROR=$(sed -e '/.*: OK!/d' <(norminette))
+			if [[ $NORM_ERROR == "" ]] ; then
+				success "\nNORMINETTE OK\n"
+			else
+				warning "\nNORMINETTE ERROR"
+				echo "$NORM_ERROR\n"
+			fi
+
+			PROG="./fractol"
+			rm -f "$PROG"
+			make
+			if [ ! -f "$PROG" ]; then
+				warning "COMPILATION FAILED"
+			else
+				success "COMPILATION OK"
+				valgrind --leak-check=full --track-origins=yes --log-file=leaks.log -s $PROG
+			fi
+		fi
+		sleep 0.1
+	done
+}
+
+get_state() {
+	if [[ $(uname) == "Linux" ]];  then
+		MD5="md5sum"
+	else
+		MD5="md5"
+	fi
+	echo $(find -L . -type f -name "*.[ch]" -exec $MD5 {} \;)
+}
+
+watch
