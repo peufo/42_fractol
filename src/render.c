6bd9f93d38c57f9ece227aab4864992a57864422
jonas <jonas.voisard@gmail.com>
2025-01-02 16:10:12 +0100

    don't print background

--- src/render.c
+++ src/render.c
@@ -6,7 +6,7 @@
 /*   By: jvoisard <jonas.voisard@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 22:50:22 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/12/23 15:03:05 by jvoisard         ###   ########.fr       */
+/*   Updated: 2025/01/02 15:59:35 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,11 +31,6 @@ int	render(t_m *m)
 	if (!m->rendering_requested)
 		return (0);
 	m->rendering_requested = 0;
-	img_draw_square(
-		m,
-		(t_dot){0, 0},
-		(t_dot){WINDOW_W, WINDOW_H},
-		0xFFFFFF);
 	view_draw(m, get_color);
 	mlx_put_image_to_window(m->mlx, m->win, m->img.data, 0, 0);
 	return (1);
